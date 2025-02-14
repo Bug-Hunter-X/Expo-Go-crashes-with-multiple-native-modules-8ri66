c296c584cb22c9197834840cf26c2f765f3fe734
BugHunter-X <askzenai@gmail.com>
2025-02-14 15:10:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,5 @@
+The solution is to avoid using Expo Go for testing apps with a high number of native modules. Instead, use one of the following methods:
+
+1. **Use Expo managed workflow:** This allows you to build your app using Expo's build service, which has more resources and can handle complex native modules more effectively.
+2. **Use EAS Build:** This is a similar service to Expo's build service and addresses the same issue.
+3. **Test on a physical device:** This will provide a more reliable testing environment than Expo Go.
